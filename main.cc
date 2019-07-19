4b9d2113f143a53867b03c6a29b3ebc70e5adae7
Callum Moseley <callum.moseley1@gmail.com>
2019-07-19 19:19:18 -0400

    stuff

--- main.cc
+++ main.cc
@@ -5,5 +5,13 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
     Tournament* t = new RoundRobin();
+
+    t->createFromPlayers(vector<string>{
+            "Ibrahim",
+            "Callum",
+            "Max",
+            "Bob"
+        });
+
     delete t;
 }
--- roundrobin.h
+++ roundrobin.h
@@ -1,23 +1,58 @@
 #ifndef RR_H
 #define RR_H
 
+#include <algorithm>
 #include "tournament.h"
 
 using namespace std;
 
 class RoundRobin : public Tournament {
+    vector<Player> players;
+    vector<Match> matches;
+
+    size_t curMatch;
 
 public:
-    RoundRobin();
+    RoundRobin(): curMatch{0} {};
+    virtual ~RoundRobin() {};
 
-    virtual void addCompetitor(string name, int seed=rand());
-    virtual void addGameScore(int matchId, int score1, int score2);
-    virtual void createTournament();
+    void addCompetitor(string name, int seed) {
+        players.emplace_back(name, seed);
+    }
+    void removeCompetitor(string name) {
+        for (auto it = players.begin(); it != players.end(); ++it) {
+            if (it->name == name) {
+                players.erase(it);
+                return;
+            }
+        }
+    }
+    void addGameScore(int matchId, int score1, int score2) {}
 
-    virtual Match& getNextMatch();
+    void createTournament() {
+        int matchId = 0;
 
-    virtual void writeToFile(string fname);
-    virtual void readToFile(string fname);
+        sort(players.begin(), players.end());
+        for (size_t i = 0; i < players.size() - 1; i++) {
+            for (size_t j = 1; j < players.size(); j++) {
+                matches.emplace_back(
+                        players[i],
+                        players[j],
+                        matchId++);
+            }
+        }
+    }
+
+    bool hasNextMatch() {
+        return curMatch >= matches.size();
+    }
+
+    Match& getNextMatch() {
+        return matches[curMatch];
+    }
+
+    void writeToFile(string fname) {}
+    void readToFile(string fname) {}
 };
 
 #endif
