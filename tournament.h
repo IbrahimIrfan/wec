1137af10e7d1f84b845b485b61ebbd237acdd885
Callum Moseley <callum.moseley1@gmail.com>
2019-07-19 19:43:44 -0400

    did stuff

--- tournament.h
+++ tournament.h
@@ -9,9 +9,10 @@
 using namespace std;
 
 struct Player {
-    Player(string n, int s) : name{n}, seed{s} {}
+    Player(string n, int s) : name{n}, seed{s}, wins{0} {}
     string name;
     int seed;
+    int wins;
 
     bool operator<(Player& other) const{
         return seed < other.seed;
@@ -54,83 +55,81 @@ class Match {
     Player p2;
 
     int matchId;
+    int firstTo;
 
     vector<Game> games;
 
+    int p1wins() {
+        int q = 0;
+        for (Game g : games) {
+            if (g.getWinner() == 1) {
+                ++q;
+            }
+        }
+        return q;
+    }
+    int p2wins() {
+        int q = 0;
+        for (Game g : games) {
+            if (g.getWinner() == 2) {
+                ++q;
+            }
+        }
+        return q;
+    }
+    int ties() {
+        int q = 0;
+        for (Game g : games) {
+            if (g.isDraw()) {
+                ++q;
+            }
+        }
+        return q;
+    }
+
 public:
-    Match(Player p1, Player p2, int matchId) : p1{p1}, p2{p2}, matchId{matchId} {}
+    Match(Player p1, Player p2, int matchId, int firstTo=1) : p1{p1}, p2{p2}, matchId{matchId}, firstTo{firstTo} {}
     ~Match() {};
 
     // Adds score for next game
     void addGameScore(int score1, int score2) {
-        games.emplace_back(score1, score2);
+        if (!isOver()) {
+            games.emplace_back(score1, score2);
+        }
     }
 
-    // Gets winner, or ""
-    int getWinner() {
-        int p1win = 0;
-        int p2win = 0;
-        int tie = 0;
+    int getMatchId() { return matchId; }
 
-        for (Game& g : games) {
-            if (g.isDraw()) { ++tie;
-            } else if (g.getWinner() == 1) { ++p1win;
-            } else { ++p2win; }
+    int winner() {
+        if (!isOver()) return 0;
+        if (p1wins() > p2wins()) {
+            return 1;
+        } else {
+            return 2;
         }
-
-        if (p1win > p2win) { return 1; }
-        else if (p2win > p1win) { return 2; }
         return 0;
     }
-
-    int getLoser() {
-        int p1win = 0;
-        int p2win = 0;
-        int tie = 0;
-
-        for (Game& g : games) {
-            if (g.isDraw()) { ++tie;
-            } else if (g.getWinner() == 1) { ++p1win;
-            } else { ++p2win; }
+    int loser() {
+        if (!isOver()) return 0;
+        if (p1wins() < p2wins()) {
+            return 1;
+        } else {
+            return 2;
         }
-
-        if (p1win < p2win) { return 1; }
-        else if (p2win < p1win) { return 2; }
-        return 0;
     }
 
-    bool isDraw() {
-        int p1win = 0;
-        int p2win = 0;
-        int tie = 0;
-
-        for (Game& g : games) {
-            if (g.isDraw()) { ++tie;
-            } else if (g.getWinner() == 1) { ++p1win;
-            } else { ++p2win; }
-        }
-
-        return p1win == p2win;
-    }
-
-    int getMatchId() {
-        return matchId;
-    }
-
-    Player getP1() {
-        return p1;
-    }
-    Player getP2() {
-        return p2;
-    }
-
+    Player getP1() { return p1; }
+    Player getP2() { return p2; }
     Player getPlayer(int p) {
         if (p == 1) return p1;
         return p2;
     }
 
-    bool isCompetitor(Player p) {
-        return p == p1 || p == p2;
+    bool isCompetitor(Player p) { return p == p1 || p == p2; }
+
+    bool isOver() {
+        return p1wins() >= firstTo ||
+               p2wins() >= firstTo;
     }
 
     friend ostream& operator<<(ostream& os, Match& m);
@@ -172,6 +171,7 @@ public:
     virtual void addGameScore(int matchId, int score1, int score2) = 0;
     virtual void createTournament() = 0;
 
+    virtual bool isOver() = 0;
     virtual bool hasNextMatch() = 0;
     virtual Match& getNextMatch() = 0;
 
